589590ecac1d8710fbc44de0af598208b3ef505d
maxximus87 <maxxturner10@gmail.com>
2016-09-13 16:25:02 -0400

    secondtest

---
+++ coinchanger.rb
@@ -0,0 +1,7 @@
+def coin_changer(cents_received)
+	coins = {}
+	if cents_received == 1
+	coins[:penny] = 1
+	end
+	coins
+end
--- coinchangerTest.rb
+++ coinchangerTest.rb
@@ -1,8 +1,13 @@
 require "minitest/autorun"
 require_relative "coinchanger.rb"
 
-class TestCoinCHanger < Minitest::Test
+class TestCoinChanger < Minitest::Test
 	def test_0_cents_return_0
-		our_money = {}
+		cents_received = 0
+		assert_equal({}, coin_changer(cents_received))
+	end
+	def test_1_cent_returns_1
+		cents_received = 1
+		assert_equal({:penny => 1}, coin_changer(cents_received))
 	end
 end
