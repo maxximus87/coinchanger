6fc95dd9fa136ef127fc14022b8c165ac2859ee6
maxximus87 <maxxturner10@gmail.com>
2016-09-13 16:51:02 -0400

    all 5 tests working

--- coinchanger.rb
+++ coinchanger.rb
@@ -1,12 +1,17 @@
 def coin_changer(cents_received)
 	coins = {}
-	if cents_received.between?(0, 4)
-	coins[:penny] = cents_received
-elsif cents_received == 5
-	coins[:nickel] = cents_received
-elsif cents_received.between?(6, 9)
-	coins[:nickel] = 5
-	coins[:penny] = cents_received - 5
+	if cents_received == 0
+		coins = {}
+	elsif 
+		cents_received.between?(0, 4)
+		coins[:penny] = cents_received
+	elsif 
+		cents_received == 5
+		coins[:nickel] = cents_received
+	elsif 
+		cents_received.between?(6, 9)
+		coins[:nickel] = 5
+		coins[:penny] = cents_received - 5
 	end
 	coins
 end
