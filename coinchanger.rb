4c0eccecd3aa02aa2574e04ae425479607f5fde0
maxximus87 <maxxturner10@gmail.com>
2016-09-13 16:41:11 -0400

    got a nickel to work!

--- coinchanger.rb
+++ coinchanger.rb
@@ -2,6 +2,8 @@ def coin_changer(cents_received)
 	coins = {}
 	if cents_received.between?(0, 4)
 	coins[:penny] = cents_received
+elsif cents_received == 5
+	coins[:nickel] = cents_received
 	end
 	coins
 end
